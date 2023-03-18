f9e311077cf841e48478dc37938e8d15ea17f1f7
Daniel <joon130693@gmail.com>
2023-03-18 22:52:55 +0900

    added Menu option

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,6 +1,11 @@
 
 public class MenuManager {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("1. Find Book Code");
+		System.out.println("2. Add Book Code");
+		System.out.println("3. Edit Book Code");
+		System.out.println("4. Location of Book Code");
+		System.out.println("5. Exit");
+		
 	}
 }
