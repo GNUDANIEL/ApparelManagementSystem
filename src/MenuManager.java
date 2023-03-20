b3713b5898acd538c403e98d6ceaad6bd5274cb7
Daniel <joon130693@gmail.com>
2023-03-20 21:37:24 +0900

    remove Library Manager System, add Apparel Management System

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,14 +1,13 @@
 
 public class MenuManager {
 	public static void main(String[] args) {
-		System.out.println("1. Find Book Code");
-		System.out.println("2. Add Book Code");
-		System.out.println("3. Edit Book Code");
-		System.out.println("4. Location of Book Code");
-		System.out.println("5. Borrowing");
-		System.out.println("6. Return");
-		System.out.println("7. Borrowing Extension");
-		System.out.println("8. Exit");
+		System.out.println("1. Location of Aparrel");
+		System.out.println("2. Add Aparrel");
+		System.out.println("3. Edit Aparrel Code");
+		System.out.println("4. Aparrel Stock");
+		System.out.println("5. Sale Applied");
+		System.out.println("6. Size of Aparrel");
+		System.out.println("7. Exit");
 		
 	}
 }
