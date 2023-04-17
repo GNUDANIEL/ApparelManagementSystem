9705e374dc55f53fbed68b647c68da3666858710
Daniel <joon130693@gmail.com>
2023-04-17 21:43:22 +0900

    removed show sizeInfo and other unnecessary menu, change switch into if else. added array list

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,118 +1,45 @@
 import java.util.Scanner;
 
-public class MenuManager {
-	public static void addApparel() {
-		Scanner sc1 = new Scanner(System.in);
-		System.out.println("Add Apparel");
-		System.out.println("Apparel Code Form: Gender, Type, Number");
-		System.out.print("Apparel Code: ");
-		String gender = sc1.next();
-		String type = sc1.next();
-		int Code = sc1.nextInt();
-		System.out.println("Apparel Code: "+ gender + " "+ type + " "+ Code);
-		sc1.close();
-	}
-	
-	public static void editApparel() {
-		Scanner sc1 = new Scanner(System.in);
-		System.out.println("Edit Apparel");
-		System.out.println("Apparel Code Form: Gender, Type, Number");
-		System.out.print("Apparel Code: ");
-		
-		String gender = sc1.next();
-		String type = sc1.next();
-		int Code = sc1.nextInt();
-		System.out.println("Apparel Code: "+ gender + " "+ type + " "+ Code);
-		sc1.close();
-	}
-	public static void sizeOfApparel() {
-		Scanner sc1 = new Scanner(System.in);
-		System.out.println("Size of Apparel");
-		System.out.println("Apparel Code Form: Gender, Type, Number");
-		System.out.print("Apparel Code: ");
-		
-		String gender = sc1.next();
-		String type = sc1.next();
-		int Code = sc1.nextInt();
-		System.out.println("Apparel Code: "+ gender + " "+ type + " "+ Code);
-		sc1.close();
-	}
-	
-	
+public class MenuManager {		
 	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
 		
-		Scanner sc = new Scanner(System.in);
+		ApparelManager apparelManager = new ApparelManager(input);
 		
 		int num=0;
 		
-		while(num != 6) {
-			System.out.println("1. Location of Apparel"); // Male section, Female Seceion > outer , Top, Bottom, Acc >
+		while(num != 5) {
+			//옷 정보에 저장할 정보: 사이즈 종류 코드 
+			System.out.println("1. Show a Apparel Information"); 
 			System.out.println("2. Add Apparel");//입력한 옷 정보를 담을 스킬이 없어 생략 아마 상속 혹은 메소듣 이용할 것이라고 예상
 			System.out.println("3. Edit Apparel Code");//저장된 정보를 불러내어 수정하는 기술 필요
-			System.out.println("4. Size of Apparel");
-			System.out.println("5. Show a Menu");
-			System.out.println("6. Exit");
-			System.out.print("Select a number between 1-6: ");
-			num = sc.nextInt();
+			System.out.println("4. Delete Apparel Code");
+			System.out.println("5. Exit");
+			System.out.print("Select a number between 1-5: ");
+			num = input.nextInt();
 			System.out.println(" ");
 			 
-			 switch (num) {
-			case 1:	
-				System.out.println("1. Male section");
-				System.out.println("2. Female Seceion");
-				System.out.print("Select a number between 1-2: ");
-				int sectionNumber1 = sc.nextInt();
-				System.out.println(" ");
-				
-				if (num == 1) {
-					System.out.println("1. outer");
-					System.out.println("2. Top");
-					System.out.println("3. Bottom");
-					System.out.println("4. Acc");
-					System.out.print("Select a number between 1-4: ");
-					int typeOfMaleApparel = sc.nextInt();
-					System.out.println(" ");
-					if (typeOfMaleApparel==1) {
-						System.out.println("Male Area 1");
-					}else if (typeOfMaleApparel==2) {
-						System.out.println("Male Area 2");
-					}else if (typeOfMaleApparel==3) {
-						System.out.println("Male Area 3");
-					}else if (typeOfMaleApparel==4) {
-						System.out.println("Male Area 4");
-					}
-					
-				}else if (num==2){
-					System.out.println("1. outer");
-					System.out.println("2. Top");
-					System.out.println("3. Bottom");
-					System.out.println("4. Acc");
-					System.out.print("Select a number between 1-4: ");
-					int typeOfFemaleApparel = sc.nextInt();
-					System.out.println(" ");
-					if (typeOfFemaleApparel==1) {
-						System.out.println("Female Area 1");
-					}else if (typeOfFemaleApparel==2) {
-						System.out.println("Female Area 2");
-					}else if (typeOfFemaleApparel==3) {
-						System.out.println("Female Area 3");
-					}else if (typeOfFemaleApparel==4) {
-						System.out.println("Female Area 4");
-					}
-				}
-				break;
-			case 2:
-				addApparel();
-				break;
-			case 3:
-				editApparel();
-				break;
-			case 4:
-				sizeOfApparel();
-				break;
-				
+			 if (num==1){
+			apparelManager.infoOfApparel();
+			 }
+			 else if(num==2) {
+			apparelManager.addApparel();
+			 }
+			 else if(num==3){
+				apparelManager.editApparel();
+			 }
+			 else if(num==4){
+				apparelManager.deleteApparel();
 			}
-			 sc.close();
+			 else if(num==5){
+					System.out.println("Program is finished");
+				}
+			 else{ 
+				 continue;
+			 }
+		
+			}
+		input.close();
 		}
 	}
-}
